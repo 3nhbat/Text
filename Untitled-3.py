0f9fe79ca6bee48e4686257ea9caec2c6c9361a0
Enhbat <zenitsug25@gmail.com>
2025-03-12 14:39:10 +0800

    Update Untitled-3.py

--- Untitled-3.py
+++ Untitled-3.py
@@ -1,26 +1,8 @@
-try:
-    pens = float(input("Үзэгний багцын тоо: "))
-    markers = float(input("Маркерын багцын тоо: "))
-    detergent = float(input("Угаалгын нунтаг (литрээр): "))
-    
-    if pens < 0 or markers < 0 or detergent < 0:
-        print("Тоо хэмжээ тэгээс бага байж болохгүй!")
-    else:
-
-        pen_price = 5.80  
-        marker_price = 7.20  
-        detergent_price = 1.20  
-        
-        total_pens = pens * pen_price
-        total_markers = markers * marker_price
-        total_detergent = detergent * detergent_price
-    
-        total_cost = total_pens + total_markers + total_detergent
-        
-        print(f"\nҮзэгний нийт үнэ: {total_pens:.2f} ам.доллар")
-        print(f"Маркерын нийт үнэ: {total_markers:.2f} ам.доллар")
-        print(f"Угаалгын нунтагны нийт үнэ: {total_detergent:.2f} ам.доллар")
-        print(f"Нийт төлбөр: {total_cost:.2f} ам.доллар")
-        
-except ValueError:
-    print("Дахин оруулна уу!")
+balnii_vne = 5.80  
+markar_vne = 7.20  
+nuntag_vne = 1.20  
+bal = int(input(""))  
+marker = int(input(""))  
+nuntag = int(input("")) 
+niit = (bal * balnii_vne) + (marker *markar_vne) + (nuntag * nuntag_vne)
+print(niit)
