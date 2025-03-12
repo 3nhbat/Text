d0894c628764ba0c5c4330af5e030254bd6437e0
Enhbat <zenitsug25@gmail.com>
2025-03-12 14:56:53 +0800

    Update Untitled-5.py

--- Untitled-5.py
+++ Untitled-5.py
@@ -1,14 +1,7 @@
-try:
-    time1 = int(input("Эхний тамирчны цаг (1-50 секунд): "))
-    time2 = int(input("Хоёр дахь тамирчны цаг (1-50 секунд): "))
-    time3 = int(input("Гурав дахь тамирчны цаг (1-50 секунд): "))
-    if not (1 <= time1 <= 50 and 1 <= time2 <= 50 and 1 <= time3 <= 50):
-        print("Цагууд 1-ээс 50 секундын хооронд байх ёстой!")
-    else:
-        total_seconds = time1 + time2 + time3
-        minutes = total_seconds // 60
-        seconds = total_seconds % 60х
-        seconds_str = f"{seconds:02d}"
-        print(f"{minutes}:{seconds_str}")
-except ValueError:
-    print("Зөвхөн бүхэл тоо оруулна уу!")
+a = int(input()) 
+b = int(input()) 
+c = int(input()) 
+niit = a + b + c
+minut = niit // 60
+second = niit % 60
+print(f"{minut:02}:{second:02}")
